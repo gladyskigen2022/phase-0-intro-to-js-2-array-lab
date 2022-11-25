e903f1098b7759955c8b162e5c9997908f934b61
GladysKigen <gladys.kigen@student.moringa.com>
2022-11-25 23:59:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+//this is like cloning an original list
+function appendCat(name){
+let newCatName = cats.slice()
+newCatName.push(name)
+return newCatName
+}
+function prependCat(name){
+    let newCatName = cats.slice()
+newCatName.unshift(name)
+return newCatName
+}
+function removeLastCat(){
+    let newCatName = cats.slice()
+    newCatName.pop()
+    return newCatName 
+}
+function removeFirstCat(){
+    let newCatName = cats.slice()
+    newCatName.shift()
+    return newCatName 
+}
